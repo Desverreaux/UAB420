99eedab83135e7de33d5d847bebaf7f20d8cd27d
nplumeri <nikolasplum@gmail.com>
2026-03-19 15:36:00 -0500

    Altered element layout and added some placeholders for UI features.

--- frontend/Vue-Project/src/App.vue
+++ frontend/Vue-Project/src/App.vue
@@ -1,11 +1,22 @@
 <template> <!-- HTML -->
 	<div class="page">
-    <header class="header">Thirsty Plant</header>
+    <header class="header">
+      <div class="title">Thirsty Plant</div>
+      <div class="UIElements">
+        <span>Guide</span>
+        <span>Search</span>
+        <span>PFP</span>
+      </div>
+    </header>
+    <button @click="guide">Guide</button>
 		
-    <main class="content">
+    <main class="main">
+      <textarea test={{ message }}></textarea>
+      <!--
       <section class="main">{{ message }}</section>
 		  <button @click="changeMessage">Change Message</button>
-      <aside class="sidebar">Your Ad Here!</aside>
+       <aside class="sidebar">Your Ad Here!</aside>
+       -->
     </main>
 	</div>
 </template>
@@ -41,23 +52,42 @@ Dark Spruce - #14591D
 Dry Sage - #A6B07E
 Smoky Rose - #8B635C
 Firey Teracotta - #D1603D
-
 */
-.container {
-  width: 100%;
-  /*max-width: 1200px;*/
-  margin: 0 auto;
-}
 
 .page {
-  display: grid;
-  grid-template-rows: auto 1fr auto; /*auto = distributes columns automatically, _fr sets a fixed size element*/
+  display: flex;
+  flex-direction: column;
   height: 100vh; /*__vh = percentage of viewport height*/
 	background-color: #14591D;
 }
 
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+
+.title{
+  font-family: "Merriweather";
+  font-size: 4rem; /* pm = pixels, em/% = relative to parent element (default is 1em = 16px), rem = relative to html element --> Can also use xx-small to xx-large*/
+  font-weight: bold; /*lighter, normal, bold, bolder*/
+  font-style: oblique 10deg; /*normal, italic, etc*/
+  text-decoration: underline; /*Can be customized*/
+
+  color: #a6b07e
+}
+
+.UIElements {
+  display: flex;
+  gap: 1rem;
+}
+
 .main {
-  padding: 25px;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   /*
   gap --> space between grid items
   padding --> space inside elements
@@ -65,25 +95,10 @@ Firey Teracotta - #D1603D
   */
 }
 
-.content {
-  gap: 20px;
-  display: grid;
-  grid-template-columns: 250px 1fr;
-}
-
-
-.header {
-  position: sticky;
-  top: 0;
-  text-align: left;
-
-  font-family: "Merriweather";
-  font-size: 4em; /* pm = pixels, em/% = relative to parent element (default is 1em = 16px), rem = relative to html element --> Can also use xx-small to xx-large*/
-  font-weight: bold; /*lighter, normal, bold, bolder*/
-  font-style: oblique 10deg; /*normal, italic, etc*/
-  text-decoration: underline; /*Can be customized*/
-
-  color: #a6b07e
+textarea {
+  width: 65%;
+  height: 200px;
+  font-size: 1rem;
 }
 
 /*
