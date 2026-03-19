232fc78cb884da1d1e3d924f4a0158ca8cdb0e6b
nplumeri <nikolasplum@gmail.com>
2026-03-18 23:59:58 -0500

    Experimenting with text attributes

--- frontend/Vue-Project/src/App.vue
+++ frontend/Vue-Project/src/App.vue
@@ -1,13 +1,14 @@
-<template>
+<template> <!-- HTML -->
 	<div>
-		<h1>{{ message }}</h1>
+		<h1>Thirsty Plant</h1>
+    <h2>{{ message }}</h2>
 		<button @click="changeMessage">Change Message</button>
 	</div>
 </template>
 
 <script>
 
-export default {
+export default { // JavaScript
 	data() {
 		return {
 			message: "Test Output"
@@ -29,8 +30,19 @@ export default {
 
 </script>
 
-<style>
+<style> /* CSS */
 body {
 	background-color: #5f464b;
+}
+
+h1 {
+  font-family: "Merriweather";
+  font-size: 4em; /* pm = pixels, em/% = relative to parent element (default is 1em = 16px), rem = relative to html element --> Can also use xx-small to xx-large*/
+  font-weight: bold; /*lighter, normal, bold, bolder*/
+  font-style: oblique 10deg; /*normal, italic, etc*/
+  text-align: left;
+  text-decoration: underline; /*Can be customized*/
+
+  color: #a6b07e
 }
 </style>
