f17012d3434f3394a111927bc6e79d7e5f7f3e89
John Desverreaux <thisisjohnmail@gmail.com>
2026-04-01 16:14:15 +0000

    separated git pull functionality into a separate fastapi script, and set up a dotenv file to obfuscate database connection creditials

---
+++ backend/gitWebhook.py
@@ -0,0 +1,31 @@
+import hmac 
+import hashlib
+import subprocess 
+from dotenv import load_dotenv 
+from fastapi import FastAPI, Request, HTTPException 
+
+load_dotenv()  # Load environment variables from .env file
+gitApp = FastAPI() # Create FastAPI app
+
+WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+REPO_PATH = os.getenv("REPO_PATH")
+
+@gitApp.post("/webhook") # GitHub webhook endpoint
+async def webhook(request: Request):
+    # Verify the request is actually from GitHub
+    signature = request.headers.get("X-Hub-Signature-256")
+    body = await request.body()
+    
+    expected = "sha256=" + hmac.new(
+        WEBHOOK_SECRET.encode(),
+        body,
+        hashlib.sha256
+    ).hexdigest()
+    
+    if not hmac.compare_digest(signature, expected):
+        raise HTTPException(status_code=401, detail="Invalid signature")
+    
+    # Run git pull
+    subprocess.run(["git", "restore", "."], cwd=REPO_PATH) # Restore the repo to discard any local changes before pulling
+    subprocess.run(["git", "pull"], cwd=REPO_PATH)
+    return {"status": "pulled"}
--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,6 @@ from fastapi.middleware.cors import CORSMiddleware # Is required to allow cross-
 # dotenv: Used for loading environment variables from a .env file, which allows you to keep sensitive information like API keys out of your codebase and easily manage them in a separate file.
 # fastapi: Used for creating the FastAPI app, defining API endpoints, handling incoming requests, and raising HTTP exceptions when needed.              
 
-WEBHOOK_SECRET = "secret"
 REPO_PATH = "/sharedFiles/Server"
 
 load_dotenv()  # Load environment variables from .env file
@@ -33,28 +32,6 @@ app.add_middleware(
 ################
 # API ENDPOINTS
 ################
-
-
-@app.post("/webhook") # GitHub webhook endpoint
-async def webhook(request: Request):
-    # Verify the request is actually from GitHub
-    signature = request.headers.get("X-Hub-Signature-256")
-    body = await request.body()
-    
-    expected = "sha256=" + hmac.new(
-        WEBHOOK_SECRET.encode(),
-        body,
-        hashlib.sha256
-    ).hexdigest()
-    
-    if not hmac.compare_digest(signature, expected):
-        raise HTTPException(status_code=401, detail="Invalid signature")
-    
-    # Run git pull
-    subprocess.run(["git", "restore", "."], cwd=REPO_PATH) # Restore the repo to discard any local changes before pulling
-    subprocess.run(["git", "pull"], cwd=REPO_PATH)
-    return {"status": "pulled"}
-
 
 @app.get("/api/hello")
 async def hello():
