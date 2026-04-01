fed1befdaf5356fbad95f862ba250e96a89bc29c
John Desverreaux <thisisjohnmail@gmail.com>
2026-04-01 17:05:55 +0000

    ahhhh

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"], # allows all headers, you can specify which headers are allowed if needed
 )
 
-#poop from a butt
+#poop from a butts
 
 ################
 # API ENDPOINTS
