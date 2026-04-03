94db35b605a7bd919a5b81f842fd122f5fbb3a87
John Desverreaux <thisisjohnmail@gmail.com>
2026-04-03 17:58:13 +0000

    made a button3

--- frontend/Vue-Project/src/App.vue
+++ frontend/Vue-Project/src/App.vue
@@ -6,8 +6,7 @@
         <button class="Guide" @click="openModal('Guide')">Guide</button>
         <button class="Search" @click="openModal('Search')">Search</button>
         <div>
-          <a href="uab420.desverreaux.com:8978" target="_blank" rel="noopener">Vue Docs</a>
-          <router-link to="/contact">Contact Page</router-link>
+          <a href="http://uab420.desverreaux.com:8978" target="_blank" rel="noopener">Database</a>
         </div> 
          <button class="PFP" @click="openModal('PFP')">👤</button>
       </div>
