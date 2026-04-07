e794c0d51eacd0f68757289a1cb9af5432f3c72a
John Desverreaux <thisisjohnmail@gmail.com>
2026-04-07 18:43:00 +0000

    fixed indentation 2

--- backend/validation.py
+++ backend/validation.py
@@ -29,8 +29,8 @@ def auto_validate(func):
 
 def validateTimeStamp(timestamp): #todo finish this function, as it currently consolidates different ways of writting a timestamp but doesn't actually check if it is a time stamp
 	try:
-	   return parser.parse(timestamp)
-   except (TypeError, ValueError):
+		return parser.parse(timestamp)
+    except (TypeError, ValueError):
 		raise HTTPException(status_code=400, detail="Bad request: invalid timestamp")
 
 def validateMoistureLevel(moistureLevel): 
