c6c900fe7f6a6ee8d75317cedd2b9e78f6034522
John Desverreaux <thisisjohnmail@gmail.com>
2026-04-07 18:37:54 +0000

    cleaning up code 2

--- backend/validation.py
+++ backend/validation.py
@@ -29,7 +29,7 @@ def auto_validate(func):
 
 def validateTimeStamp(timestamp): #todo finish this function, as it currently consolidates different ways of writting a timestamp but doesn't actually check if it is a time stamp
     try:
-       return = parser.parse(timestamp)
+       return parser.parse(timestamp)
    except (TypeError, ValueError):
         raise HTTPException(status_code=400, detail="Bad request: invalid timestamp")
 
