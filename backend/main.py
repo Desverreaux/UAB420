2f9d36439f8719829e48139bd5e345b2721c478a
John Desverreaux <thisisjohnmail@gmail.com>
2026-04-09 16:00:31 +0000

    fixed 6

--- backend/main.py
+++ backend/main.py
@@ -88,7 +88,7 @@ async def get_plant_data(plantIdentifier):
 
 	data = db.getPlantData(plantIdentifier)
 	
-	return {"data": fakeModels.plant(plantID=1)}
+	return {"data": fake.plant(plantID=1)}
 	# return {"data": data}
 
 
